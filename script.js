fa479a6d08f03b683215547093fcc9158bc3efdf
Kevin <generationfiery95@gmail.com>
2020-09-17 15:25:43 +0800

    Changed work experience order"

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-    // Closes responsive menu when a scroll trigger link is clicked
+    // Closes responsive menu when a navbar link is clicked
     $(".navbar-nav>li>a").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    $(".navbar-brand").click(function () {
         $(".navbar-collapse").collapse("hide");
     });
